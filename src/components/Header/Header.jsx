a769e7f1dfdbfa61bf5a8b0c565deedde9fe0562
Jhumberto <jhumbertto@gmail.com>
2023-04-28 10:12:14 -0600

    Header using BEM

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,26 @@ import './header.scss'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav className='header'>
+      <a href='/' className='headerLogo'>LOGO</a>
+      <ul className='headerNavList'>
+        <li className='headerListItem'>
+        <a href='/' className='HeaderItemLink'>Home</a>
+          </li>
+        <li className='headerListItem'>
+        <a href='/dashboard' className='HeaderItemLink'>Dashboard</a>
+          </li>
+        <li className='headerListItem'>
+        <a href='/secret' className='HeaderItemLink'>Secret</a>
+          </li>
+        <li className='headerListItem'>
+        <a href='/login' className='HeaderItemLink'>Login</a>
+          </li>
+        <li className='headerListItem'>
+        <a href='/singup' className='HeaderItemLink'>Singup</a>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
