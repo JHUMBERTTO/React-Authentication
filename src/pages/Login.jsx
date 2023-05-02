2761dd556544f990f8eaab174b5710a08664570f
Jhumberto <jhumbertto@gmail.com>
2023-05-02 12:54:16 -0600

    UI login using Boostrap

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,24 @@
-
+import '../assets/css/form.css'
 const Login = () => {
   return (
-    <h1>Login</h1>
+    <div>
+      <main className="form-signin w-100 m-auto">
+        <form>
+          <img className="mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"/>
+          <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+          <div className="form-floating">
+            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
+            <label htmlFor="floatingInput">Email address</label>
+          </div>
+          <div className="form-floating">
+            <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
+            <label htmlFor="floatingPassword">Password</label>
+          </div>
+          <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+          <p className="mt-5 mb-3 text-body-secondary">© 2017–2023</p>
+        </form>
+      </main>
+    </div>
   )
 }
 
